3d1fa6ee19cc4b24bb0ba6d71c1cb2784af9abec
Mary Racter <paracetamolboy@gmail.com>
2014-06-11 11:05:16 +0200

    network visualisation of kids in a schoolyard with random powers of attraction/repulsion, for dat babby tutorial

--- babby_steps/Student.java
+++ babby_steps/Student.java
@@ -1,11 +1,14 @@
 import sim.engine.*;
 import sim.field.continuous.*;
 import sim.util.*;
+import sim.field.network.*;
 
 //implementing Steppable means this object can be called by the Scheduler of a simulation
 public class Student implements Steppable
 {
- 
+
+ public static final double MAX_FORCE = 3.0;
+
  public void step(SimState state)
  {
   //apparently implementing steppable means you have a dependency on this object's calling context class variables
@@ -17,7 +20,49 @@ public class Student implements Steppable
 
   //mutable 2D location class (used to store the result of applying forces to this agent??)
   MutableDouble2D sumForces = new MutableDouble2D();
+ 
+  //Go through buddies and determine forces of attraction/repulsion
 
+  MutableDouble2D forceVector = new MutableDouble2D();
+
+  Bag out = students.buddies.getEdges(this, null);
+
+  int len = out.size();
+
+  //go through all the outgoing nodes and do some shit upon it
+  for (int buddy = 0; buddy < len; buddy++)
+  {
+   Edge e = (Edge) (out.get(buddy));
+   double buddiness = ((Double) (e.info)).doubleValue();
+
+   //getotherNode() gets the 'buddy' student on the opposite end of this one
+
+   Double2D other = students.yard.getObjectLocation(e.getOtherNode(this));
+
+   //get closer to buddy if not close enough already
+   if (buddiness  >= 0 )
+   {
+    forceVector.setTo((other.x - me.x) * buddiness, (other.y - me.y) * buddiness);
+
+    //temper dat max force
+	if (forceVector.length() > MAX_FORCE)
+     forceVector.resize(MAX_FORCE);
+
+   }
+   //get further away from buddy if too close
+   else
+   {
+    forceVector.setTo((other.x - me.x) * buddiness, (other.y - me.y) * buddiness);
+
+	if (forceVector.length() > MAX_FORCE)
+     forceVector.resize(0.0);  
+    else if (forceVector.length() > 0)
+	 forceVector.resize(MAX_FORCE - forceVector.length()); //invert distance
+   }
+
+   sumForces.addIn(forceVector);
+
+  }
 
   sumForces.addIn(new Double2D((yard.width * 0.5 - me.x) * students.forceToSchoolMultiplier,
 				 (yard.height * 0.5 - me.y) * students.forceToSchoolMultiplier));
--- babby_steps/Students.java
+++ babby_steps/Students.java
@@ -1,6 +1,7 @@
 import sim.engine.*;
 import sim.util.*;
 import sim.field.continuous.*;
+import sim.field.network.*;
 
 public class Students extends SimState
 {
@@ -10,6 +11,7 @@ public class Students extends SimState
 
  public double forceToSchoolMultiplier = 0.01;
  public double randomMultiplier = 0.1;
+ public Network buddies = new Network (false);
 
  public Students (long seed)
  {
@@ -23,7 +25,7 @@ public class Students extends SimState
 
   yard.clear();
 
-//  buddies.clear();
+  buddies.clear();
 
   for (int i = 0; i < numStudents; i++)
   {
@@ -31,9 +33,42 @@ public class Students extends SimState
    
    yard.setObjectLocation(student, new Double2D (yard.getWidth() * 0.5 + random.nextDouble() - 0.5,
    												 yard.getHeight() * 0.5 + random.nextDouble() -0.5));
-
+   
+   //add students to the network
+   buddies.addNode(student);
    schedule.scheduleRepeating(student); 
 
+  }
+
+  //define like/dislike relationships to scatter students away/bring them close
+
+  Bag students = buddies.getAllNodes();
+
+  for (int i = 0; i < students.size(); i++)
+  {
+   Object student = students.get(i);
+
+   //define who this student likes
+   Object studentB = null;
+
+   double buddiness = random.nextDouble();
+   
+   do
+   {
+    studentB = students.get(random.nextInt(students.numObjs));
+    buddies.addEdge(student, studentB, new Double(buddiness));
+   }
+   while (student == studentB);
+
+
+   //define who this student dislikes
+   do
+   {
+    studentB = students.get(random.nextInt(students.numObjs));
+    buddiness = random.nextDouble();
+    buddies.addEdge(student, studentB, new Double(-buddiness));
+   }
+   while (student == studentB);
   }
 
  }
